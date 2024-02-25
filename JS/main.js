26d738257e696fca12325dcc0ea725edff7fa5e4
Ahmed Mohamed Abo-Alata <62518605+ahmedAboalata1@users.noreply.github.com>
2024-02-26 00:11:31 +0200

    Finish Html,CSS

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+let title=document.getElementById('title')
+let price=document.getElementById('price')
+let ads=document.getElementById('ads')
+let discount=document.getElementById('tax')
+let total=document.getElementById('total')
+let category=document.getElementById('category')
+let count=document.getElementById('count')
+let submit=document.getElementById('submit')
+console.log("🚀", submit,count,category,tax,discount,title,price, ads)
+
+function fun() {
+    total.innerHTML=123
+}
